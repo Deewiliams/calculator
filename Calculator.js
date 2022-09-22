c8df00edb96e9eac1d5e36bf90478d930dc3d4e4
Desire <desireirankundawilliams@gmail.com>
2022-09-22 22:07:17 +0200

    added functionality

---
+++ Calculator.js
@@ -0,0 +1,17 @@
+const userInput = document.querySelector(".userInput");
+const options = document.querySelectorAll(".option");
+const equals = document.getElementById("equals");
+const results = document.querySelector(".results");
+
+function doCalculation() {
+  results.innerHTML = eval(userInput.value);
+  userInput.value = "";
+}
+equals.addEventListener("click", doCalculation);
+
+for (let i = 0; i < options.length; i++) {
+  options[i].addEventListener("click", (e) => {
+    //   userInput.value = userInput.value + e.target.value;
+    userInput.value += e.target.value;
+  });
+}
